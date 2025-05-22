eeed76b0894bc59fe180d43b85a9e4691d2ddeb6
elvishn20 <elvis1042.rodriguez@gmail.com>
2025-05-22 04:23:05 -0600

    Creacion y manejo de formularios con Django

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import ProductForm
 
-# Create your views here.
+
+class ProductFormView(generic.FormView):
+    template_name = "products/add_product.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product') # Si el producto se crea hace una redireccion a esa url
+
+
+    def form_valid(self, form): # Llama al metodo del formulario
+        form.save()
+        return super().form_valid(form)
+    
