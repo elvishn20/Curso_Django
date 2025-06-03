a2b7483aaae009584d09a01d85701015354c9998
elvishn20 <elvis1042.rodriguez@gmail.com>
2025-06-03 15:33:52 -0600

    Manejo de superusuario en django

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+    list_display = ['name', 'price'] # Aparece los campos de nombre y precio en el administrador de django.
+    search_fields = ['name'] # Agrega un buscador en el administrador de django.
+
+admin.site.register(Product, ProductAdmin)
