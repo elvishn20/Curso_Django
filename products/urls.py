49d76e341a8a7b63677a54364bc175d403c657b7
elvishn20 <elvis1042.rodriguez@gmail.com>
2025-06-03 16:15:35 -0600

    Creacion de la aplicacion de usuarios

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ProductFormView, ProductListView
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name="list_view"),
+    path('', ProductListView.as_view(), name="list_product"),
     path('agregar/', ProductFormView.as_view(), name="add_product"),
 ]
